6841bd729e0114ec98a6be0ed4b08e49476e0d3a
Christie Bahlai <cbahlai@kent.edu>
2026-04-15 13:34:08 -0400

    move Grage project to Bahlailab

--- Site_combined_figures.R
+++ Site_combined_figures.R
@@ -46,15 +46,41 @@ all_heat.site
 dev.off()
 
 
+#ok, this is going to be nuts because we're combining 3, 9 panel figures
 
-allccf.sbcdata
-allderived.sbcdata
 
-complete_analysis.sbcdata<-plot_grid(timeseriesplot.sbcdata, all_heat.sbcdata, allccf.sbcdata, allderived.sbcdata, 
-                                     ncol=2,  rel_heights=c(1,1,1,1), labels="AUTO", axis="b", align="v")
+allccf.site<-plot_grid(allccf.hubbarddata, allccf.konzadata, allccf.sbcdata,  
+                                     ncol=1,  rel_heights=c(1,1,1), labels="AUTO", axis="b", align="v")
 
-complete_analysis.sbcdata
+allccf.site
 
-pdf("figures/sbcfig1.pdf", width = 14.5, height = 9)
-complete_analysis.sbcdata
+pdf("figures/allccf.site.pdf", width = 8, height = 15)
+allccf.site
+dev.off()
+
+
+#and now for the allderived plot
+allderived.hubbarddata1<-allderived.hubbarddata+ 
+  theme(legend.text = element_blank(),
+        legend.title = element_blank(),
+        legend.key = element_blank(),
+        legend.background = element_rect(fill = "transparent", colour = NA))+
+  guides(color = guide_none(), fill = guide_none(), shape = guide_none(), linetype = guide_none())
+
+allderived.sbcdata1<-allderived.sbcdata+ 
+  theme(legend.text = element_blank(),
+        legend.title = element_blank(),
+        legend.key = element_blank(),
+        legend.background = element_rect(fill = "transparent", colour = NA))+
+  guides(color = guide_none(), fill = guide_none(), shape = guide_none(), linetype = guide_none())
+
+
+
+allderived.site<-plot_grid(allderived.hubbarddata1, allderived.konzadata, allderived.sbcdata1,  
+                       ncol=1,  rel_heights=c(1,1,1), labels="AUTO", axis="b", align="v")
+
+allderived.site
+
+pdf("figures/allderived.site.pdf", width = 8, height = 12)
+allderived.site
 dev.off()
